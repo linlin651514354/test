8bdf76720f98bd983246c54d4ff25069ac1df733
linlin651514354 <31949632+linlin651514354@users.noreply.github.com>
2018-05-31 19:12:48 +0800

    Update hello.py

--- lalalla/hello.py
+++ lalalla/hello.py
@@ -6,3 +6,4 @@ f.close()
 print "I will write the source codes of baidu.com into the file named baidu.txt"
 print "I will write the source codes of baidu.com into the file named baidu.txt"
 print "I will write the source codes of baidu.com into the file named baidu.txt"
+print "I will write the source codes of baidu.com into the file named baidu.txt"
